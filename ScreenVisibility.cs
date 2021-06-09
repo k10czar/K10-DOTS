41a5c324f962ebc40248536647ded6565b508e08
Gabriel Leite <glimaleite@gmail.com>
2021-06-09 18:03:44 -0300

    removed some code from ScreenVisibility that was supposed to be on CharacterScreenVisibility

--- ScreenVisibility.cs
+++ ScreenVisibility.cs
@@ -9,17 +9,6 @@ public class ScreenVisibility : MonoBehaviour
 	private readonly BoolState _isVisible = new BoolState( true );
 	public IBoolStateObserver IsVisible => _isVisible;
 
-	private readonly Semaphore _canShow = new Semaphore();
-	public ISemaphore CanShow => _canShow;
-
-	private readonly StateRequester _lockValue = new StateRequester();
-	public IStateRequesterInfo LockValueState => _lockValue;
-	private bool _lockedValue;
-
-	private void Awake()
-	{
-		_canShow.OnFalseState.Register( () => UpdateVisibility(false) );
-	}
 
 	void OnEnable()
 	{
@@ -32,26 +21,9 @@ public class ScreenVisibility : MonoBehaviour
 		_isVisible.SetFalse();
 	}
 
-	public void SetData( float3 viewportPosition, bool isVisible, bool ignoreCanShow = false )
+	public void SetData( float3 viewportPosition, bool isVisible )
 	{
 		_viewportPosition = viewportPosition;
-
-		bool becomeVisible = _lockValue.Requested ? _lockedValue : ( isVisible && CanShow.Free) || (isVisible && ignoreCanShow);
-		UpdateVisibility( becomeVisible );
-	}
-
-	void UpdateVisibility(bool becomeVisible )
-	{
-		_isVisible.Setter( becomeVisible );
-	}
-
-	public void LockValue(bool lockedValue, object key)
-	{
-		_lockValue.Request( key );
-		_lockedValue = lockedValue;
-	}
-
-	public void RemoveLockValue(object key){
-		_lockValue.RemoveRequest( key );
+		_isVisible.Setter( isVisible );
 	}
 }
